f1ce5093b98d2f111f25a88debafea9f5b3192c5
David Durieux <david@durieux.family>
2025-07-27 21:04:58 +0200

    Continue game

--- stardeception/1_login_page/1_login_page.gd
+++ stardeception/1_login_page/1_login_page.gd
@@ -2,4 +2,5 @@ extends CanvasLayer
 
 
 func _on_button_pressed() -> void:
+#	TODO Call HTTP request to auth server to authenticate
 	get_tree().change_scene_to_file("res://2_main_page/main_page.tscn")
