42a9c57414fcd6ff43fea1959b3266e91fab4993
Neoz Sagan <51477498+Neoz-JourneyLab@users.noreply.github.com>
2025-08-14 15:29:10 +0200

    Localization https://docs.godotengine.org/en/4.4/tutorials/i18n/internationalizing_games.html
    fix requesteds
    
    **   - Il y a de fichiers .tmp qui ont été commit par erreur je pense. Pareil pour le wordlist.json qui n'est pas utilisé ?**
    Je ne sais pas trop a quoi ils servent, effectivement on peut les shooter.
    Pour le wordlist.json également il est obsolète
    j'ai délete
       **- Quand on est en mode automatique le select est vide, il faudrait pas rajouter une entrée "auto" ou sélectionner automatiquement la langue actuelle ?**
    c'était bien la langue détectée par défaut mais le drowdown ne se mettait pas à jour, j'ai fix
    **   - Pour les fichiers de traduction on part sur des csv et pas des .po ?**
    ça ce n'est pas à moi de décider, mais ça ne change strictement rien sur la manière d'organiser les scènes.
    Le PO est généré selon le code et les champs textes tout comme le .CSV de manière auto.
    Si quelqu'un tranche je peux préparer l'exemple pour utiliser un .PO

---
+++ tools/localization/localization_manager.gd
@@ -0,0 +1,50 @@
+extends Node
+
+@export var dropdown: OptionButton
+var localization_index: Dictionary
+
+func _ready() -> void:
+	for id in dropdown.item_count:
+		localization_index[dropdown.get_item_text(id)] = id
+	var language = "automatic"
+	
+	if FileAccess.file_exists("user://settings.json"):
+		var file := FileAccess.open("user://settings.json", FileAccess.READ)
+		var content := file.get_as_text()
+		file.close()
+		var json: Dictionary = JSON.parse_string(content)
+		if typeof(json) != TYPE_DICTIONARY:
+			print("Fichier JSON invalide")
+		else :
+			language = json["language"]
+	
+	if language == "automatic":
+		var preferred_language = OS.get_locale_language()
+		print("Set language to " + preferred_language)
+		TranslationServer.set_locale(preferred_language)
+		print(tr("GREET"))
+		language = preferred_language
+	else:
+		TranslationServer.set_locale(language)
+	
+	dropdown.text = language
+	dropdown.select(localization_index[language])
+
+
+func _on_option_button_item_selected(index: int) -> void:
+	TranslationServer.set_locale(dropdown.get_item_text(index))
+	print("Language set to " + dropdown.get_item_text(index))
+	var json: Dictionary
+	if FileAccess.file_exists("user://settings.json"):
+		var file := FileAccess.open("user://settings.json", FileAccess.READ)
+		var content := file.get_as_text()
+		file.close()
+		json = JSON.parse_string(content)
+		if typeof(json) != TYPE_DICTIONARY:
+			print("Fichier JSON invalide")
+			
+	json["language"] = dropdown.get_item_text(index)
+	var json_out := JSON.stringify(json, "\t")
+	var file_out := FileAccess.open("user://settings.json", FileAccess.WRITE)
+	file_out.store_string(json_out)
+	file_out.close()
