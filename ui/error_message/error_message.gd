17b93c13a283b86433c442f1a9d505b19c99e67a
GuillaumeCa <guicarre1@gmail.com>
2025-08-20 01:14:11 +0200

    Add an error message scene

---
+++ ui/error_message/error_message.gd
@@ -0,0 +1,54 @@
+extends CanvasLayer
+
+class_name ErrorMessage
+
+@export var title: String
+@export var message: String
+@export var exit_button_label: String
+@export var exit_scene: PackedScene
+
+@onready var background: ColorRect = $ErrorMessage/Background
+@onready var error_container: VBoxContainer = $ErrorMessage/VBox
+@onready var title_label: Label = $ErrorMessage/VBox/Panel/VBoxContainer/Title
+@onready var message_label: Label = $ErrorMessage/VBox/Panel/VBoxContainer/Message
+@onready var exit_btn: Button = $ErrorMessage/VBox/ExitBtn
+
+
+signal exited
+
+func _ready() -> void:
+	title_label.text = title
+	message_label.text = message
+	exit_btn.text = exit_button_label
+
+	hide()
+	background.color.a = 0
+	error_container.modulate.a = 0
+	error_container.scale = Vector2.ONE * 0.4
+
+
+func show_message(_title: String = "", _message: String = ""):
+	if _title != "":
+		title_label.text = _title
+	if _message != "":
+		message_label.text = _message
+	
+	show()
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	var tw = create_tween()
+	tw.parallel().tween_property(background, "color:a", 0.9, 0.5).set_trans(Tween.TRANS_CUBIC)
+	tw.parallel().tween_property(error_container, "modulate:a", 1, 0.5).set_trans(Tween.TRANS_CUBIC)
+	tw.parallel().tween_property(error_container, "scale", Vector2.ONE, 1).set_trans(Tween.TRANS_CUBIC)
+
+func hide_message():
+	var tw = create_tween()
+	tw.parallel().tween_property(background, "color:a", 0, 0.5).set_trans(Tween.TRANS_CUBIC)
+	tw.parallel().tween_property(error_container, "modulate:a", 0, 0.5).set_trans(Tween.TRANS_CUBIC)
+	tw.parallel().tween_property(error_container, "scale", Vector2.ONE * 0.4, 1).set_trans(Tween.TRANS_CUBIC)
+	await tw.finished
+	hide()
+
+func _on_exit_btn_pressed() -> void:
+	exited.emit()
+	get_tree().change_scene_to_packed(exit_scene)
+	hide_message()
--- ui/login_page/login_page.gd
+++ ui/login_page/login_page.gd
@@ -2,7 +2,7 @@ extends Control
 
 func _on_ready() -> void:
 	BackgroundMusic.play_music_level()
-	pass
+	
 
 
 func _on_button_pressed(button_id: String) -> void:
