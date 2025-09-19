3989b81c189b8ec813b990194237fddc4e1579ac
kazord <kazord@gmail.com>
2025-09-20 00:50:55 +0200

    Update grid.gd
    fix for gravity

--- scenes/grid/grid.gd
+++ scenes/grid/grid.gd
@@ -6,3 +6,11 @@ class_name PhysicsGrid
 
 # func _physics_process(delta: float) -> void:
 # 	gravity_direction = -global_basis.y
+
+
+#this fix gravity when transform the node (local direction instead of worldwide gravity definiting) 
+@export var local_gravity_direction := Vector3(0, -1, 0)
+
+func _notification(what: int):
+	if what == NOTIFICATION_TRANSFORM_CHANGED:
+		gravity_direction = global_transform.basis * local_gravity_direction
